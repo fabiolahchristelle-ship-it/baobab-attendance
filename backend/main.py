47fd8f43adc63104f7feae2cf1916e1351baa998
Fabiolah2002 <fabiolah.christelle@gmail.com>
2025-10-08 23:34:10 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -25,6 +25,11 @@ app = FastAPI(
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
 # 🌐 CORS pour APK, navigateur, etc.
 app.add_middleware(
     CORSMiddleware,
