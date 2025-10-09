5fa0a90c4030c026e018ca35414ad8df97d48d5c
Fabiolah2002 <fabiolah.christelle@gmail.com>
2025-10-09 20:49:31 +0300

    Update Index.jsx

--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -210,7 +210,7 @@ export default function Index() {
       </div>
 
       <div id="qr-reader" style={{ display: 'none' }}>
-        <div className="qr-overlay"></div>
+        
       </div>
 
       <div className="clock">
